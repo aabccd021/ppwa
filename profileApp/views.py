2336621199fe7eed4505419227652e6a01b9a5d8
aabccd021 <aabccd021@gmail.com>
2018-09-27 13:19:54 +0700

    go my app please go

--- profileApp/views.py
+++ profileApp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'profileApp/index.html', {})
+
+def hobby(request):
+    return render(request, 'profileApp/hobby.html', {})
+
+def register(request):
+    return render(request, 'profileApp/buku_tamu.html', {})
